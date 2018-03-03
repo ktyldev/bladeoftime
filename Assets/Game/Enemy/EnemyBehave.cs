19ffb5fa840183cb6dca763a038c24201322040c
Cat Flynn <cathalflynn@hotmail.co.uk>
2018-03-03 06:22:26 +0000

    Enemy attack range

--- Assets/Game/Enemy/EnemyBehave.cs
+++ Assets/Game/Enemy/EnemyBehave.cs
@@ -4,12 +4,13 @@ using UnityEngine;
 using Extensions;
 
 public class EnemyBehave : MonoBehaviour {
+
+    [SerializeField]
+    private float _speed;
+    [SerializeField]
+    private float _attackDistance;
+
     private Transform _player;
-    public float speed;
-
-    
-    
-
 
     // Use this for initialization
     void Start () {
@@ -19,6 +20,11 @@ public class EnemyBehave : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         transform.LookAt(_player);
-        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        transform.Translate(Vector3.forward * _speed * Time.deltaTime);
+
+        if (Vector3.Distance(transform.position, _player.position) < _attackDistance)
+        {
+            // print("enemy in range!");
+        }
     }
 }
