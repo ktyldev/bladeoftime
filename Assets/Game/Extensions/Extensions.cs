dcd463c810a168f94aad1e808d56d04b7c159639
Cat Flynn <cathalflynn@hotmail.co.uk>
2018-03-03 01:12:40 +0000

    Add extension methods

---
+++ Assets/Game/Extensions/Extensions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Extensions
+{
+    public static class Extensions
+    {
+        public static GameObject Find(this MonoBehaviour script, string tag)
+        {
+            var result = GameObject.FindGameObjectWithTag(tag);
+            if (result == null)
+                throw new Exception();
+
+            return result;
+        }
+
+        public static T Find<T>(this MonoBehaviour script, string tag)
+            where T : MonoBehaviour
+        {
+            var result = Find(script, tag)
+                .GetComponent<T>();
+
+            if (result == null)
+                throw new Exception();
+
+            return result;
+        }
+
+        public static T FindInChild<T>(this MonoBehaviour script, string tag)
+        {
+            var result = Find(script, tag)
+                .GetComponentInChildren<T>();
+
+            if (result == null)
+                throw new Exception();
+
+            return result;
+        }
+    }
+}
