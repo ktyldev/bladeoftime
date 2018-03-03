af935efa4d1d6569f653a009a22b8922f3c37a90
cyndrdev <34170110+cyndrdev@users.noreply.github.com>
2018-03-03 18:51:06 +0000

    added enemy spawner

---
+++ Assets/Game/Enemy/EnemySpawner.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+public class EnemySpawner : Spawner
+{
+    [SerializeField]
+    private float _spawnFrequencyMax;
+    [SerializeField]
+    private float _spawnFrequencyMin;
+
+    private float _spawnFrequency;
+    protected override bool CanSpawn(GameObject template)
+    {
+        return true;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            Spawn();
+        }
+    }
+
+    private void Awake()
+    {
+        _spawnFrequency = _spawnFrequencyMax;
+        StartCoroutine(SpawnRoutine());
+    }
+
+    IEnumerator SpawnRoutine()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(_spawnFrequency);
+            Spawn();
+            _spawnFrequency = Mathf.Clamp(
+                _spawnFrequency * .98f,
+                _spawnFrequencyMin,
+                _spawnFrequencyMax
+            );
+            print("spawn frequency: " + _spawnFrequency);
+        }
+    }
+}
---
+++ Assets/Game/General/Spawner.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+public abstract class Spawner : MonoBehaviour
+{
+    public GameObject[] templates;
+    public float minDistanceFromPlayer;
+
+    protected abstract bool CanSpawn(GameObject template);
+
+    protected virtual GameObject Spawn()
+    {
+        var playerTransform = GameObject.Find(GameTags.Player).transform;
+
+        var angle = UnityEngine.Random.rotation.eulerAngles.y;
+        var dir = new Vector3(Mathf.Sin(Mathf.Deg2Rad * angle), 0, Mathf.Cos(Mathf.Deg2Rad * angle));
+        var offset = dir * minDistanceFromPlayer;
+        var spawnPos = playerTransform.position + offset;
+
+        var options = templates
+            .Where(CanSpawn)
+            .ToArray();
+
+        return Instantiate(
+            options[UnityEngine.Random.Range(0, options.Count())],
+            spawnPos,
+            Quaternion.identity,
+            null);
+    }
+}
