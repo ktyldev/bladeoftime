6bdb9859c1110d15414dd19dfba8dfcd3c120956
cyndrdev <34170110+cyndrdev@users.noreply.github.com>
2018-03-03 02:48:54 +0000

    added gamepad controls! (probably)

---
+++ Assets/Game/Input/Gamepad.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using Extensions;
+
+public class Gamepad : MonoBehaviour, IControlMode
+{
+    bool _lastFire;
+
+    public Vector3 AimDirection { get { return GetAimDirection(); } }
+    public Vector3 MoveDirection { get { return GetMoveDirection(); } }
+    public UnityEvent Melee { get; private set; }
+    public UnityEvent Fire { get; private set; }
+    public bool IsAiming { get { return Input.GetAxisRaw("AimTrigger") > .3f; } }
+
+    void Awake()
+    {
+        Melee = new UnityEvent();
+        Fire = new UnityEvent();
+    }
+
+    void Update()
+    {
+        if (Input.GetAxisRaw("Fire") > 0.3f)
+        {
+            if (IsAiming && !_lastFire)
+            {
+                _lastFire = true;
+                Fire.Invoke();
+            }
+        }
+        else
+        {
+            _lastFire = false;
+        }
+
+        if (Input.GetButtonDown("Melee"))
+        {
+            Melee.Invoke();
+        }
+    }
+
+    private Vector3 GetMoveDirection()
+    {
+        var h = Input.GetAxis("Horizontal");
+        var v = Input.GetAxis("Vertical");
+
+        return new Vector3(h, 0, v);
+    }
+
+    private Vector3 GetAimDirection()
+    {
+        if (IsAiming) return GetMoveDirection();
+        var h = Input.GetAxis("RightH");
+        var v = Input.GetAxis("RightV");
+
+        return new Vector3(h, 0, v);
+    }
+}
