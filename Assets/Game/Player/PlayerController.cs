db788e36c5f559c8b56546f1ef1958d654407593
Cat Flynn <cathalflynn@hotmail.co.uk>
2018-03-03 16:21:40 +0000

    Cone for hitting enemies

--- Assets/Game/Player/PlayerController.cs
+++ Assets/Game/Player/PlayerController.cs
@@ -25,7 +25,10 @@ public class PlayerController : MonoBehaviour
     private float _meleeDistance;
     [SerializeField]
     private float _meleeConeAngle;
+    [SerializeField]
+    private float _attackTime;
 
+    private bool _isAttacking;
     private IControlMode _input;
     private Vector3 _momentum;
     private Quaternion _lookRotation;
@@ -54,6 +57,9 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
+        if (_isAttacking)
+            return;
+
         Aim();
         Move();
     }
@@ -73,26 +79,72 @@ public class PlayerController : MonoBehaviour
             return;
         bool isStill = _input.MoveDirection == Vector3.zero;
         var targetDirection = isStill ? _input.AimDirection : _input.MoveDirection;
-        var targetRotation = (targetDirection == Vector3.zero) ?
-            transform.rotation :
-            Quaternion.LookRotation(targetDirection, Vector3.up);
-
+        var targetRotation = (targetDirection == Vector3.zero) ? transform.rotation : Quaternion.LookRotation(targetDirection, Vector3.up);
+        
         _lookRotation = Quaternion.Lerp(transform.rotation, targetRotation, _rotateSensitivity);
         transform.rotation = _lookRotation;
     }
 
+    private void Aim(Vector3 dir)
+    {
+        var lookAtPos = transform.position + dir;
+        transform.LookAt(lookAtPos);
+    }
+
     private void Melee()
     {
-        if (_isDashing)
+        if (_isAttacking || _isDashing)
             return;
-        print("melee!");
+
+        StartCoroutine(MeleeAttack());
     }
 
     private void Fire()
     {
-        if (_isDashing)
+        if (_isAttacking || _isDashing)
             return;
+
+        StartCoroutine(FireAttack());
+    }
+
+    
+    private IEnumerator MeleeAttack()
+    {
+        _isAttacking = true;
+        print("melee!");
+        Aim(_input.AimDirection);
+
+        // cast from ~the middle of the player
+        var ray = new Ray(transform.position + Vector3.up, _input.AimDirection);
+        var hitObjects = Physics.SphereCastAll(ray, 1f) // this radius parameter doesn't seem to work :/
+            .Where(h =>
+            {
+                return Vector3.Angle(_input.AimDirection, h.transform.position - transform.position) < _meleeConeAngle / 2f;
+            })
+            .Where(h =>
+            {
+                return Vector3.Distance(transform.position, h.transform.position) < _meleeDistance; // Hacky distance
+            })
+            .Select(h => h.collider.gameObject.GetComponent<Health>())
+            .Where(h => h != null)
+            .ToArray();
+
+
+        foreach (var hit in hitObjects)
+        {
+            print(hit.gameObject);
+        }
+        yield return new WaitForSeconds(_attackTime);
+        _isAttacking = false;
+    }
+
+    private IEnumerator FireAttack()
+    {
+        _isAttacking = true;
         print("fire!");
+        Aim(_input.AimDirection);
+        yield return new WaitForSeconds(_attackTime);
+        _isAttacking = false;
     }
 
     private void Dash()
