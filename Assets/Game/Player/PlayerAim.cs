ea373682850f3f15529022ba5888974d09cd6b08
Cat Flynn <cathalflynn@hotmail.co.uk>
2018-03-03 18:09:51 +0000

    Trigger aim

--- Assets/Game/Player/PlayerAim.cs
+++ Assets/Game/Player/PlayerAim.cs
@@ -2,15 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerAim : MonoBehaviour {
+public class PlayerAim : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _lineRenderer;
+    [SerializeField]
+    private float _lineLength;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private LineRenderer _line;
+    private IControlMode _input;
+    private bool _active;
+
+    void Start()
+    {
+        _input = GetComponentInChildren<IControlMode>();
+        _line = _lineRenderer.GetComponent<LineRenderer>();
+        if (_line == null || _input == null)
+            throw new System.Exception();
+    }
+
+    void Update()
+    {
+        if (_input.IsAiming && !_active)
+        {
+            _active = true;
+            print("aim start");
+        }
+        if (!_input.IsAiming && _active)
+        {
+            _active = false;
+            print("aim end");
+        }
+    }
 }
