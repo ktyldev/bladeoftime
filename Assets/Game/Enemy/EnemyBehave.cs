6336ee8bfd8bf6c054d075c0ddbbe2906728fe2f
Zalaman <dcdarkninja573@gmail.com>
2018-03-03 05:36:36 +0000

    Enemy creation and movement

---
+++ Assets/Game/Enemy/EnemyBehave.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Extensions;
+
+public class EnemyBehave : MonoBehaviour {
+    private Transform _player;
+    public float speed;
+
+    
+    
+
+
+    // Use this for initialization
+    void Start () {
+        _player = this.Find(GameTags.Player).transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.LookAt(_player);
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+}
