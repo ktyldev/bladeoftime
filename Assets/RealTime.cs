4a9b3cc519d42045655a08d2746e66da193e929f
Cat Flynn <cathalflynn@hotmail.co.uk>
2018-03-04 20:29:00 +0000

    BIG UI :ok_hand:

---
+++ Assets/RealTime.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RealTime : MonoBehaviour
+{
+    [SerializeField]
+    private Text _text;
+    [SerializeField]
+    private float _scale;
+
+    private float _current;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+        _current += Time.deltaTime * _scale;
+    }
+
+    // Update is called once per frame
+    void OnGUI()
+    {
+        _text.text = _current.ToString();
+    }
+}
